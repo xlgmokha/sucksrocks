c4e2915620ee5d5281a5ea09ae1731061a087c07
mo k <mo@mokhan.ca>
2012-05-21 08:44:52 -0600

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rockssucks::Application.config.secret_token = '386e7ec3b47a5a1afeb00ad41cd59853d3db55c6012475611c1c5488fe12348fce68ae726f6179bca6abdf0a2aa57b3837afba9ae457f3b1ffdf2aa002b39d4d'
